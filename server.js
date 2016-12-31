339f53267e42472462529a446502a399338b521f
John Clark <johnclark@Johns-MacBook-Pro.local>
2016-12-31 15:28:35 -0500

    When a search is entered in the url, send an https request to google images api and use JSON.parse to parse the json string, return json response with image results and wanted info

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var https = require('https')
+var http = require('http')
+var API_KEY = 'AIzaSyDRwhq1mt9KyLBySU_66k6f4s4W8Ohy-s8'
+var SEARCH_ID = '015375743846369420198:cnraqfuetko'
 
 var app = express()
 
@@ -7,11 +10,36 @@ app.get('/', function (req,res) {
   res.send("Welcome to the Image Search App. Add /api/imagesearch/[your search] to the url to see results.")
 })
 
-app.get('/api/imagesearch/:search', function (req,res) {
-
+app.get('/api/imagesearch/:search', function (req,response) {
+  var results = []
+  var images = [];
   var search = req.params.search
   var offset = req.query.offset || 0
 
+
+  var url = `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${SEARCH_ID}&q=${search}&searchType=image&fileType=jpg&imgSize=medium&num=2`
+  var req = https.request(url, (res) => {
+    res.on('data', (chunk) => {
+      images.push(chunk)
+    }).on('end', () => {
+      var body = Buffer.concat(images)
+      var imagesObj = JSON.parse(body)
+
+      imagesObj.items.forEach( (item) => {
+        var returnJSON = {
+          "url": item.link,
+          "alt-text": item.snippet,
+          "context-url": item.image.contextLink,
+          "thumbnail": item.image.thumbnailLink
+        }
+        results.push(returnJSON)
+      })
+
+      response.send(results)
+    });
+
+  });
+  req.end();
 
 
 })
