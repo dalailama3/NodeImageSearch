82162dc86a43804da43834bac7942bfd8505aa1d
John Clark <johnclark@Johns-MacBook-Pro.local>
2016-12-31 14:09:53 -0500

    Add https to package.json and write a route for when a search word is entered

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express')
+var https = require('https')
 
 var app = express()
 
 app.get('/', function (req,res) {
-  res.end("Hello World!")
+  res.send("Welcome to the Image Search App. Add /api/imagesearch/[your search] to the url to see results.")
+})
+
+app.get('/api/imagesearch/:search', function (req,res) {
+
+  var search = req.params.search
+  var offset = req.query.offset || 0
+
+
+
 })
 
 app.listen(3000)
